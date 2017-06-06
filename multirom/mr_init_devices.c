c1a04863fe44a47e085f212b2c91ca5018d9897f
Haikal Izzuddin <heywhite69@gmail.com>
2017-06-06 11:51:14 +0800

    ferrari: Fix mr paths

--- multirom/mr_init_devices.c
+++ multirom/mr_init_devices.c
@@ -10,25 +10,25 @@ const char *mr_init_devices[] =
     "/sys/class/graphics/fb0",
 
     "/sys/block/mmcblk0",
-    "/sys/devices/soc/7824900.sdhci",
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0",
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001",
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p37", // boot
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p52", // cache
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p19", // modem
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p30", // persist
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p53", // system
-    "/sys/devices/soc/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p54", // userdata
+    "/sys/devices/soc.0/7824900.sdhci",
+    "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0",
+    "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001",
+    "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
+    "/sys/devices/soc.0c/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p30", // boot
+    "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p35", // cache
+    "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p29", // modem
+    "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p32", // persist
+    "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p34", // system
+    "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p36", // userdata
     "/sys/bus/mmc",
     "/sys/bus/mmc/drivers/mmcblk",
     "/sys/module/mmc_core",
     "/sys/module/mmcblk",
 
-    "/sys/devices/soc.0/gpio_keys.70/input*",
+    "/sys/devices/soc.0/gpio_keys.68/input*",
     "/sys/devices/virtual/input*",
     "/sys/devices/virtual/misc/uinput",
-    "/sys/devices/soc/78b8000.i2c/i2c-3/3-0020/input*",
+    "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-004a/input*",
 
     // for adb
     "/sys/devices/virtual/tty/ptmx",
